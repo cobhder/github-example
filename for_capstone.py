cc72c5dd804a03539120fe449314b76ddec776c3
123 <123>
2020-06-02 19:37:43 +0800

    adds a python script

---
+++ for_capstone.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# # this is for capstone
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print ('Hello Capstone Project Course!')
+
